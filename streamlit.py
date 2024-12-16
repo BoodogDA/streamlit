c08e985cb9cc20899b00f60f6b2a16ae02518ecd
boodogDA <sba24453@student.cct.ie>
2024-12-16 19:32:48 +0000

    streamlit tmap
    I have an intention to use Streamlit to create a dashboard for CA 2.

--- streamlit.py
+++ streamlit.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.write("this is my demo!")
+import numpy as np
+import pandas as pd
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
